c7817fb3f117ab000f5aa0a6805090b96385afe4
Nikhil Kothawade <nikhil.infy2019@gmail.com>
2020-07-01 20:53:11 +0530

    vs-code github setup

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@ import React from "react";
 import logo from "./logo.svg";
 import uuid from "uuid";
 import "bootstrap/dist/css/bootstrap.min.css";
-import TodoInput from './Components/TodoInput';
-import TodoList from './Components/TodoList'
+import TodoInput from "./Components/TodoInput";
+import TodoList from "./Components/TodoList";
 
 function App() {
+state ={
+  items:[{id:1,title:'wake up'},
+  {id:1,title:'sleep'}]
+}
+
+
+
   return (
     <div className="container">
       <div className="row">
-        <TodoInput/>
-        <TodoList/>
+        <TodoInput />
+        <TodoList />
       </div>
     </div>
   );
